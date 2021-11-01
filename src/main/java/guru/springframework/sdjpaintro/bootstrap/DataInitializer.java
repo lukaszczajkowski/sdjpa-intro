3cfb99057ad3192ff668191de1e4ca5a690d6861
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-11-01 22:28:48 +0100

    MySQL Spring Boot configuration

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -16,16 +16,18 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
+        if (bookRepository.count() == 0) {
+            Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
 
-        System.out.println("Id: " + bookDDD.getId());
+            System.out.println("Id: " + bookDDD.getId());
 
-        Book savedDDD = bookRepository.save(bookDDD);
+            Book savedDDD = bookRepository.save(bookDDD);
 
-        System.out.println("Id: " + savedDDD.getId());
+            System.out.println("Id: " + savedDDD.getId());
 
-        Book bookSIA = new Book("Spring In Action", "234234", "Oriely");
-        Book savedSIA = bookRepository.save(bookSIA);
+            Book bookSIA = new Book("Spring In Action", "234234", "Oriely");
+            Book savedSIA = bookRepository.save(bookSIA);
+        }
 
         bookRepository.findAll().forEach(book -> {
             System.out.println("Book Id: " + book.getId());
